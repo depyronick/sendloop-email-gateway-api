e4a36128c8b81d89e06410cdf597c1f044f6ec24
Ali Poder <alipoder@gmail.com>
2017-04-07 23:48:46 +0300

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Ali Poder on 7.04.2017.
- */
-var Message = require('./lib/models/Message');
-var Sendloop = require('./index.js');
-var sendLoop = new Sendloop({
-    apiKey: "1213"
-});
-sendLoop.send(1, new Message(), 1, 1, 1);
